c5bf7a54d0a41aa8bad1ff8cd1aab4bf8b01ad0a
Konrad Eisele <eiselekd@gmail.com>
2014-11-09 20:10:33 +0100

    all

--- .emacs.d/lisp/utils/compile.el
+++ .emacs.d/lisp/utils/compile.el
@@ -30,6 +30,30 @@
       (add-to-list 'cc-compilers-list "clang")
       (add-to-list 'c++-compilers-list "clang++"))))
 
+(defun find-file-upwards (file-to-find)
+  "Recursively searches each parent directory starting from the default-directory. looking for a file with name file-to-find.  Returns the path to it or nil if not found."
+  (labels
+      ((find-file-r
+	(path)
+	(let* ((parent (file-name-directory path))
+	       (possible-file (concat parent file-to-find)))
+	  (cond
+	   ((file-exists-p possible-file) possible-file) ; Found
+	   ;; The parent of ~ is nil and the parent of / is itself.
+	   ;; Thus the terminating condition for not finding the file
+	   ;; accounts for both.
+	   ((or (null parent) (equal parent (directory-file-name parent))) nil) ; Not found
+	   (t (find-file-r (directory-file-name parent))))))) ; Continue
+    (find-file-r default-directory)))
+
+(defun project-root ()
+  (let ((my-tags-file (find-file-upwards "COMPILE_CMD")))
+    (when my-tags-file
+      (message "Loading tags file: %s" my-tags-file)
+      (visit-tags-table my-tags-file))))
+  
+;;(concat "cd " (project-root) " && scons")
+
 
 (defun utils/compile-init ()
   "Initialize compile module."
