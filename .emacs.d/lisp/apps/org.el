d4ebb7472a824ad4655da92b16b0c1f02edd9e68
Konrad Eisele <eiselekd@gmail.com>
2015-03-02 18:39:00 +0100

    all

--- .emacs.d/lisp/apps/org.el
+++ .emacs.d/lisp/apps/org.el
@@ -342,13 +342,14 @@ Callers of this function already widen the buffer view."
 (setq org-refile-allow-creating-parent-nodes (quote confirm))
 
 ; Use IDO for both buffer and file completion and ido-everywhere to t
-(setq org-completion-use-ido t)
-(setq ido-everywhere t)
-(setq ido-max-directory-size 100000)
-(ido-mode (quote both))
+;;(setq org-completion-use-ido t)
+;;(setq ido-everywhere t)
+;;(setq ido-max-directory-size 100000)
+;;(ido-mode (quote both))
+
 ; Use the current window when visiting files and buffers with ido
-(setq ido-default-file-method 'selected-window)
-(setq ido-default-buffer-method 'selected-window)
+;;(setq ido-default-file-method 'selected-window)
+;;(setq ido-default-buffer-method 'selected-window)
 ; Use the current window for indirect buffer display
 (setq org-indirect-buffer-display 'current-window)
 
