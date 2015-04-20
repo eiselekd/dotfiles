4f174741b1253d5cb20d27e50b7837ed08f0a6ca
Konrad Eisele <eiselekd@gmail.com>
2015-04-20 08:28:01 +0200

    all

--- .emacs.d/lisp/utils/flycheck.el
+++ .emacs.d/lisp/utils/flycheck.el
@@ -1,10 +1,90 @@
+;; http://emacswiki.org/emacs/ElispCookbook#toc29
+;;(utils/flycheck-search-linux-makefile)
+
+(defun utils/flycheck-search-linux-makefile ()
+  "Search for linux top `Makefile' "
+  (labels
+      ((find-makefile-file-r
+	(path)
+	(let* ((parent (file-name-directory path))
+	       (file (concat parent "Makefile")))
+	  (cond
+	   ((file-exists-p file)
+	    (progn
+	      (with-temp-buffer
+		
+		(insert-file-contents file)
+		(if (string-match "VERSION = [0-9]+[[:space:]]*PATCHLEVEL" (buffer-string))
+		    (throw 'found-it parent)))))
+	   ((equal path parent) (throw 'found-it nil))
+	   (t (find-makefile-file-r (directory-file-name parent)))))))
+    (if (buffer-file-name)
+        (catch 'found-it 
+          (find-makefile-file-r (buffer-file-name)))
+      (error "buffer is not visiting a file"))))
+
+
+(flycheck-define-checker utils/flycheck-linux-makefile-checker
+  "A C/C++ syntax checker using GCC.
+
+Requires GCC 4.8 or newer.  See URL `https://gcc.gnu.org/'."
+  :command ("cd" 
+            "-fshow-column"
+            "-fno-diagnostics-show-caret" ; Do not visually indicate the source location
+            "-fno-diagnostics-show-option" ; Do not show the corresponding
+                                        ; warning group
+            "-iquote" (eval (flycheck-c/c++-quoted-include-directory))
+            (option "-std=" flycheck-gcc-language-standard concat)
+            (option-flag "-fno-exceptions" flycheck-gcc-no-exceptions)
+            (option-flag "-fno-rtti" flycheck-gcc-no-rtti)
+            (option-flag "-fopenmp" flycheck-gcc-openmp)
+            (option-list "-include" flycheck-gcc-includes)
+            (option-list "-W" flycheck-gcc-warnings concat)
+            (option-list "-D" flycheck-gcc-definitions concat)
+            (option-list "-I" flycheck-gcc-include-path)
+            (eval flycheck-gcc-args)
+            "-x" (eval
+                  (pcase major-mode
+                    (`c++-mode "c++")
+                    (`c-mode "c")))
+            source
+            ;; GCC performs full checking only when actually compiling, so
+            ;; `-fsyntax-only' is not enough. Just let it generate assembly
+            ;; code.
+            "-S" "-o" null-device)
+  :error-patterns
+  ((error line-start
+          (message "In file included from") " " (file-name) ":" line ":"
+          column ":"
+          line-end)
+   (info line-start (file-name) ":" line ":" column
+         ": note: " (message) line-end)
+   (warning line-start (file-name) ":" line ":" column
+            ": warning: " (message) line-end)
+   (error line-start (file-name) ":" line ":" column
+          ": " (or "fatal error" "error") ": " (message) line-end))
+  :error-filter
+  (lambda (errors)
+    (flycheck-fold-include-levels (flycheck-sanitize-errors errors)
+                                  "In file included from"))
+  :modes (c-mode c++-mode)
+  :next-checkers ((warning . c/c++-cppcheck)))
+
+
+
+
+
+
 
 
 ;;'(flycheck-c/c++-gcc-executable "/usr/local/bin/gcc-4.9")
 
 (defun utils/flycheck-mode-hook ()
   "Flycheck mode hook."
-  ;;; (Bindings) ;;;
+  (make-variable-buffer-local 'flycheck-linux-makefile)
+  (setq flycheck-linux-makefile (utils/flycheck-search-linux-makefile))
+  (if (flycheck-linux-makefile)
+      (flycheck-select-checker 'utils/flycheck-linux-makefile-checker))
   (utils/flycheck-local-keybind ))
 
 (defun utils/flymake-mode-hook ()
