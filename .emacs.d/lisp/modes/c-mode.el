147719118bb0107822b33f51babeaba361cc2e65
Konrad Eisele <eiselekd@gmail.com>
2015-04-24 15:25:47 +0200

    all

--- .emacs.d/lisp/modes/c-mode.el
+++ .emacs.d/lisp/modes/c-mode.el
@@ -1,2 +1,5 @@
-(provide 'modes/c-mode.el)
 
+(add-hook 'c++-mode-hook (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+(add-hook 'c-mode-hook   (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+
+(provide 'modes/c-mode.el)
