1b90becd537538930203d11aa35ae02379cd5463
eiselekd <eiselekd@gmail.com>
2015-06-22 14:15:52 +0200

    all

--- .emacs.d/lisp/modes/c-mode.el
+++ .emacs.d/lisp/modes/c-mode.el
@@ -2,13 +2,15 @@
 (add-hook 'c++-mode-hook
 	  (lambda ()
 	    (progn (add-to-list 'write-file-functions 'delete-trailing-whitespace)
-		   (ggtags-mode)
+		   (if (eq system-type 'cygwin) ;; use helm for !=cygwin
+		       (ggtags-mode))
 		   )))
 (add-hook 'c-mode-hook
 	  (lambda ()
 	    (progn
 	      (add-to-list 'write-file-functions 'delete-trailing-whitespace)
-	      (ggtags-mode)
+		   (if (eq system-type 'cygwin) ;; use helm for !=cygwin
+		       (ggtags-mode))
 	      )))
 
 (provide 'modes/c-mode.el)
