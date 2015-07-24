3dc3f856e81a967065588358acf53b4279640d36
Konrad Eisele <eiselekd@gmail.com>
2015-07-25 00:02:09 +0200

    all

--- .emacs.d/lisp/modes/c-mode.el
+++ .emacs.d/lisp/modes/c-mode.el
@@ -4,6 +4,10 @@
 	    (progn (add-to-list 'write-file-functions 'delete-trailing-whitespace)
 		   (if (eq system-type 'cygwin) ;; use helm for !=cygwin
 		       (ggtags-mode))
+		   (global-set-key (kbd "M-(")  'hs-hide-block)
+		   (global-set-key (kbd "M-)")  'hs-show-block)
+
+
 		   )))
 (add-hook 'c-mode-hook
 	  (lambda ()
