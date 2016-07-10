7c9e7fb5917b97e8ccc1933050160564a5c01abf
Konrad Eisele <eiselekd@gmail.com>
2016-07-10 13:50:07 +0000

    all

--- .vpn/setup-vpn.client.sh
+++ .vpn/setup-vpn.client.sh
@@ -8,3 +8,16 @@ apt-get install strongswan xl2tpd
 cp vpnclient.key.pem /etc/ipsec.d/private/
 cp vpnca.cert.pem /etc/ipsec.d/cacert/
 cp vpnclient.cert.pem /etc/ipsec.d/cert/
+
+echo <<EOF
+
+conn vpnconn
+      leftsubnet=192.168.0.0/16
+      leftcert=vpnclient.cert.der
+      right=$serverip
+      rightsubnet=192.168.0.0/16
+      rightid="$serverdn"
+      auto=start
+
+EOF
+
--- .vpn/setup-vpn.server.sh
+++ .vpn/setup-vpn.server.sh
@@ -11,3 +11,16 @@ cp vpnserver.key.pem /etc/ipsec.d/private/
 cp vpnca.cert.pem /etc/ipsec.d/cacert/
 cp vpnserver.cert.pem /etc/ipsec.d/cert/
 
+echo <<EOF
+
+conn vpnconn
+      left=
+      #left=%defaultroute
+      leftsubnet=192.168.0.0/16
+      leftcert=vpnserver.cert.der
+      right=%any
+      rightca="$cadn"
+      keyexchange=ikev2
+      auto=start
+
+EOF
