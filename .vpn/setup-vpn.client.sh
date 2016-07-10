b0cc08b052dd3807b6caef3fcad35b0868dc27e0
root <root@ubilinux.(none)>
2016-07-10 13:01:50 +0000

    all

--- .vpn/setup-vpn.client.sh
+++ .vpn/setup-vpn.client.sh
@@ -2,8 +2,8 @@
 
 apt-get install strongswan xl2tpd
 
-cp vpnca.cert.pem /usr/share/ca-certificates
-dpkg-reconfigure ca-certificates
+#cp vpnca.cert.pem /usr/share/ca-certificates
+#dpkg-reconfigure ca-certificates
 
 cp vpnclient.key.pem /etc/ipsec.d/private/
 cp vpnca.cert.pem /etc/ipsec.d/cacert/
--- .vpn/setup-vpn.server.sh
+++ .vpn/setup-vpn.server.sh
@@ -4,8 +4,8 @@ apt-get install strongswan xl2tpd
 
 bash vpn-cert.sh
 
-cp vpnca.cert.pem /usr/share/ca-certificates
-dpkg-reconfigure ca-certificates
+#cp vpnca.cert.pem /usr/share/ca-certificates
+#dpkg-reconfigure ca-certificates
 
 cp vpnserver.key.pem /etc/ipsec.d/private/
 cp vpnca.cert.pem /etc/ipsec.d/cacert/
