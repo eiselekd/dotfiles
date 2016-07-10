c3362deb8577a837fc5cc60446431f982cf0065f
Konrad Eisele <eiselekd@gmail.com>
2016-07-10 12:51:25 +0000

    all

--- .vpn/vpn-cert.sh
+++ .vpn/vpn-cert.sh
@@ -1,6 +1,9 @@
 set -x
 rm *der
 
+if [ -f vpn.txt ]; then
+. vpn.txt
+else
 cadn="C=SE, O=VPNNET, CN=VPNServer Root CA"
 
 serverdn="C=SE, O=VPNNET, CN=VPNServer"
@@ -12,6 +15,7 @@ clientdn="C=SE, O=VPNNET, CN=VPNClient"
 clientsan0=vpnclient.net
 clientsan1=vpnclient.net
 clientcrl=http://vpnserver.net
+fi
 
 ipsec pki --gen -s 4096 > vpnca.key.der
 ipsec pki --self --ca --lifetime 1460 --in vpnca.key.der --dn "${cadn}" > vpnca.cert.der
