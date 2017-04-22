b3a5b9c5720ed51ec9a43442a81caa6e9748b36e
Konrad Eisele <eiselekd@gmail.com>
2017-04-22 19:34:27 +0000

    all

---
+++ ubuntu/setup.sh
@@ -0,0 +1,10 @@
+# X11:
+apt update
+apt install emacs xmonad git xmobar xinit
+
+adduser eiselekd
+
+git config --global user.email "eiselekd@gmail.com"
+git config --global user.name "Konrad Eisele"
+
+
