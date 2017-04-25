0fec5b84a1289c9600e78cb8c15563916c85d90f
Konrad Eisele <eiselekd@gmail.com>
2017-04-25 14:44:57 +0000

    all

---
+++ ubuntu/grub.sh
@@ -0,0 +1,11 @@
+
+# /etc/default/grub
+# update-grub
+# grub-install /dev/disk/by-id/<id>
+
+# zfs snapshot rpool/ROOT/ubuntu@install
+# exit
+
+# mount | grep -v zfs | tac | awk '/\/mnt/ {print $3}' | xargs -i{} umount -lf {}
+# zpool export rpool
+# reboot
