c33fc25a8dc0067351cbf17d40622e498ce48ae9
Konrad Eisele <eiselekd@gmail.com>
2017-10-18 16:17:19 +0000

    all

--- .emacs.d/lisp/modes/org-mode.el
+++ .emacs.d/lisp/modes/org-mode.el
@@ -1,10 +1,19 @@
 
 (add-hook 'org-mode-hook
           (lambda ()
-	    
-	    (when (require 'ox-twbs nil t)
-	      (progn
-		(message "[+] ox-twbs")
-		))))
-
+	    (progn
+	      (message "[+] org-version: '%s'" org-version)
+	      (require 'ox)
+	      (require 'ox-html)
+	      (when (require 'ox-reveal nil t)
+		(progn
+		  
+		  (setq org-reveal-root (concat  "file://" *.emacs.d.dir* "lisp/modes/reveal.js"  ))
+		  (message "[+] ox-reveal: '%s'" org-reveal-root)
+		  ))
+	      (when (require 'ox-twbs nil t)
+		(progn
+		  (message "[+] ox-twbs")
+		  )))))
+	  
 (provide 'modes/org-mode.el)
