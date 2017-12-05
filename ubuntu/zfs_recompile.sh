da4c61bbeadb3f26c768ac293381748b4cd94ae9
Konrad Eisele <Konrad Eisele>
2017-12-05 19:39:21 +0000

    all

--- ubuntu/zfs_recompile.sh
+++ ubuntu/zfs_recompile.sh
@@ -43,6 +43,13 @@ dolinux ()
     (cd linux; DEB_BUILD_OPTIONS="debug nostrip noopt" make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION=-custom)
 }
 
+installlinux ()
+{
+    cd ~/git
+    sudo dpkg -i linux-*.deb
+    (cd linux DEB_BUILD_OPTIONS="debug nostrip noopt" make -j `getconf _NPROCESSORS_ONLN` modules_install LOCALVERSION=-custom)
+}
+
 dozfs ()
 {
     cd  zfs
@@ -54,6 +61,13 @@ dozfs ()
     { V=1 make deb 2>&1 | tee log_deb.txt } || exit 1;
 }
 
+dozfsdeb ()
+{
+    cd zfs
+    # install spl for zfs build
+    sudo dpkg -i *deb
+}
+
 dokernelrearrange ()
 {
     cd /lib/modules/<version>
@@ -63,19 +77,15 @@ dokernelrearrange ()
     depmod -a <version>
 }
 
-dozfsdeb ()
-{
-    cd zfs
-    # install spl for zfs build
-    sudo dpkg -i *deb
-}
-
 case "$1" in
     git)
 	dogit
 	;;
     linux)
 	dolinux
+	;;
+    installlinux)
+	installlinux
 	;;
     spl)
 	dospl
