54b67470e3a77918185e5377abfe5f1fc3ae32d6
Konrad Eisele <eiselekd@gmail.com>
2018-03-24 17:41:57 +0100

    all

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -284,7 +284,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;;'(custom-enabled-themes (quote (wombat)))
+ '(custom-enabled-themes (quote (wombat)))
  '(safe-local-variable-values
    (quote
     ((verilog-library-directories "." "../../cortexm7/verilog")
