a3c444a1a02a70a68185ddfe294ca4a4b81bf6ec
Konrad Eisele <eiselekd@gmail.com>
2018-08-15 23:15:00 +0200

    all

--- windows/disableamd.sh
+++ windows/disableamd.sh
@@ -1,9 +1,12 @@
 d=$1
 
 # http://vfio.blogspot.com/2015/05/vfio-gpu-how-to-series-part-3-host.html
-# echo "options vfio-pci ids=1002:68f9,1002:aa68" >> ${d}/etc/modprobe.d/vfio.conf
+#radeon: echo "options vfio-pci ids=1002:68f9,1002:aa68" >> ${d}/etc/modprobe.d/vfio.conf
+#nvidia: echo "options vfio-pci ids=10de:128b,10de:0e0f" >> ${d}/etc/modprobe.d/vfio.conf
+
 echo "install vfio-pci /sbin/vfio-pci-override-vga.sh" >> ${d}/etc/modprobe.d/vfio.conf
-echo "blacklist radeon"                                >> ${d}/etc/modprobe.d/blacklist.conf
+#echo "blacklist radeon"                                >> ${d}/etc/modprobe.d/blacklist.conf
+echo "blacklist nouveau"                               >> ${d}/etc/modprobe.d/blacklist.conf
 
 cp vfio-pci-override-vga.sh ${d}/sbin/vfio-pci-override-vga.sh
 chmod a+rwx ${d}/sbin/vfio-pci-override-vga.sh
--- windows/vfio-pci-override-vga.sh
+++ windows/vfio-pci-override-vga.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DEVS="0000:05:00.0 0000:05:00.1"
+DEVS="0000:01:00.0 0000:01:00.1"
 
 for DEV in $DEVS; do
     echo "vfio-pci" > /sys/bus/pci/devices/$DEV/driver_override
