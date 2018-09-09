ecb5fc56f03a7922512845e540e85aaf5d7b2f1d
Konrad Eisele <Konrad.Eisele@aptiv.com>
2018-09-09 17:04:53 +0200

    all

--- ubuntu/zfs_recompile.sh
+++ ubuntu/zfs_recompile.sh
@@ -87,13 +87,13 @@ function dozfs
 		--build=x86_64-linux-gnu --prefix=${p}/usr --includedir=\${prefix}/include --mandir=\${prefix}/share/man --infodir=\${prefix}/share/info --sysconfdir=${p}/etc --localstatedir=/var --libdir=\${prefix}/lib/x86_64-linux-gnu --libexecdir=\${prefix}/lib/x86_64-linux-gnu  --bindir=${p}/usr/bin --sbindir=${p}/sbin --libdir=${p}/lib --with-udevdir=${p}/lib/udev --with-systemdunitdir=${p}/lib/systemd/system --with-systemdpresetdir=${p}/lib/systemd/system-preset --with-systemdpresetdir=${p}/lib/systemd/system-preset --with-systemdgeneratordir=${p}/usr/lib/systemd/system-generators --with-systemdmodulesloaddir=${p}//usr/lib/modules-load.d --with-mounthelperdir=${p}/sbin --with-dracutdir=${p}/usr/lib/dracut/modules.d/02zfsexpandknowledge \
 
     V=1 make;
-    v=1 make install;
+    v=1 make INSTALL_MOD_PATH=${p} install;
 
-    make -C modules modules
+    #make INSTALL_MOD_PATH=${p} -C modules modules
 
-    make DESTDIR=${p} -C modules 
+    #make DESTDIR=${p} -C modules 
     
-x    #LC_TIME=C V=1 make deb;
+    #LC_TIME=C V=1 make deb;
 }
 
 function dozfsdeb
