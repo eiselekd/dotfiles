de31f5aad5e9765e476d667eab3969bedf72a5dd
Konrad Eisele <eiselekd@gmail.com>
2018-11-13 21:31:07 +0100

    all

--- windows/passthrough/start.sh
+++ windows/passthrough/start.sh
@@ -4,6 +4,17 @@ if cat /proc/cmdline | grep "efifb:off"; then
     cd /mnt/data-n0/vms-win/
     bash /mnt/data-n0/vms-win/win10_uefi_cpu_host_qemu_vm.sh -M -p -o -n -u &
     echo $! > /mnt/data-n0/vms-win/start.pid
+    (
+	# monitor qemu and reboot after exit
+	while true; do
+	    if kill -0 $(cat /mnt/data-n0/vms-win/start.pid) 2>/dev/null; then
+		true;
+	    else
+		reboot;
+	    fi
+	    sleep 1;
+	done
+    ) &
 else
     (while true; do sleep 100; done) &
     echo $! > /mnt/data-n0/vms-win/start.pid
