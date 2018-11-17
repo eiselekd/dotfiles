1b62b75d020c096a10bbd9d7df636eb4a1fc4b67
eiselekd <eiselekd@gmail.com>
2018-11-17 17:19:42 +0100

    all

--- windows/ctxmenue.sh
+++ windows/ctxmenue.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-echo "$(pwd): archive: $0 -- ${1//\\/\\\\}" >> /cygdrive/c/temp/a.txt
+echo "$(pwd): archive: $0 -- ${1//\\/\\\\} ${2}" >> /cygdrive/c/temp/a.txt
 f=$(cygpath -u "${1//\\/\\\\}")
 d=$(dirname "$f")
 b=$(basename "$f")
 echo "$(pwd): $f -- $d -- $b" >> /cygdrive/c/temp/a.txt
-( cd "$d"; tar czvf "${b}.tar.gz" "${b}" )
 
+case ${2} in
+    gz)  ( cd "$d"; tar -cvzf "${b}.tar.${2}" "${b}" );;
+    bz2) ( cd "$d"; tar -cvjf "${b}.tar.${2}" "${b}" );;
+    *)   ( cd "$d"; tar -cvf "${b}.tar" "${b}" );;
+esac
 
 
