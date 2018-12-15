928206becd57a14dbb66382414301472f1e5618c
Konrad Eisele <eiselekd@gmail.com>
2018-12-15 14:19:57 +0100

    all

--- windows/passthrough/start.sh
+++ windows/passthrough/start.sh
@@ -4,6 +4,7 @@ b=$(dirname `readlink -f $0`)
 if cat /proc/cmdline | grep "efifb:off"; then
     cd ${b}
     #-u
+    socat TCP-LISTEN:44444,fork,reuseaddr TCP:localhost:55555 &
     bash ${b}/win10_uefi_cpu_host_qemu_vm.sh -M -p -o -n -r -V  &
     echo $! > /tmp/vm-start.pid
 else
