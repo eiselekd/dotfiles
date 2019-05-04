02d54e2766294fd1903bd3cc0fc71802edb23e44
Konrad Eisele <eiselekd@gmail.com>
2019-05-04 16:53:59 +0000

    all

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -72,6 +72,8 @@
 (add-to-list 'load-path (expand-file-name "lib/js/multiple-cursors.el" *.emacs.d.lisp.dir*  ))
 (add-to-list 'load-path (expand-file-name "lib/js/yasnippet" *.emacs.d.lisp.dir*  ))
 
+
+
 (require 'cc-mode)
 (setq c-default-style
       '((java-mode . "java")
