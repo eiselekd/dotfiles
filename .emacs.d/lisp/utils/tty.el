fe3d4b9722f4017cfb7222b7245b2f83921a2b72
Konrad Eisele <eiselekd@gmail.com>
2019-10-13 12:49:15 +0200

    all

--- .emacs.d/lisp/utils/tty.el
+++ .emacs.d/lisp/utils/tty.el
@@ -13,18 +13,39 @@
 
 (defun tty-uart0-show (&rest args)
   (interactive) ;;  (and current-prefix-arg (list "--3way"))
+  (switch-to-buffer "/dev/ttyUSB0")
+  (delete-other-windows)
   )
+
 (defun tty-uart1-show (&rest args)
   (interactive) ;;  (and current-prefix-arg (list "--3way"))
+  (switch-to-buffer "/dev/ttyUSB1")
+  (delete-other-windows)
   )
+
+(defun i0-commands-show (&rest args)
+  (interactive)
+  )
+
+(defun g0-commands-show (&rest args)
+  (interactive)
+  )
+
+
 
 (define-transient-command tty-dispatch ()
   "Invoke a Magit command from a list of available commands."
-  ["Transient and dwim commands"
+  ["open and execute commands"
    [("c" "Connect"        tty-uart-connect)]
    [("1" "TTY0"           tty-uart0-show)]
    [("2" "TTY1"           tty-uart1-show)]
-   ]
+   [("i" "i0"             i0-commands-show)] ;;i0-command-open
+   [("g" "g0"             g0-commands-show)] ;;i0-command-open
+
+  ]
   )
+
+
+
 
 (provide 'utils/tty.el)
