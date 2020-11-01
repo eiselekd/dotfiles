0d63398afdf3b017f706fa88e69e2f744d17abf0
Konrad Eisele <konrad.eisele@aptiv.com>
2020-11-01 22:45:37 +0100

    all

--- bin/altunmap.sh
+++ bin/altunmap.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# rebind alt-[left|right|up|down] in virtual console
+# make part of rc.local:
+# sudo systemctl edit --full rc-local
+
+# sudo systemctl set-default multi-user.target
+# sudo systemctl set-default graphical.target
+
 echo "alt keycode 105 = F105" | loadkeys
 echo "alt keycode 106 = F106" | loadkeys
 echo "alt keycode 103 = F103" | loadkeys
