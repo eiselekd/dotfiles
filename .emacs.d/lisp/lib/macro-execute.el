85e94db762045d5ab0afd298cbe5c57b72d87c11
Konrad Eisele <eiselekd@gmail.com>
2021-09-19 12:41:06 +0200

    all

--- .emacs.d/lisp/lib/macro-execute.el
+++ .emacs.d/lisp/lib/macro-execute.el
@@ -8,15 +8,30 @@
 (require 'format-spec)
 (require 'package nil t) ; used in `magit-version'
 (require 'with-editor)
+(require 'f)
 
 (transient-define-prefix custom-command-menue ()
   "Show available macros."
   :info-manual "(magit)Macros"
   ["Macros"
-   [("0" "~/bin/la-1-2-r8.1-push-manifest.sh" (lambda () (interactive) (magit-macro-list (format "~/bin/la-1-2-r8.1-push-manifest.sh %s" default-directory))))
+   [("0" "~/bin/la-1-2-r8.1-push-manifest.sh"
+     (lambda () (interactive) (magit-macro-list (format "~/bin/la-1-2-r8.1-push-manifest.sh %s" default-directory))))
+    ("!" "switch dirs '!'" custom-dir-menue)
+    ]
+   ]
+  )
+
+(transient-define-prefix custom-dir-menue ()
+  "Show available macros."
+  :info-manual "(magit)Macros"
+  ["Switchdirs"
+   [("0" "~/bin/dir0.txt"
+     (lambda () (interactive) (dired (replace-regexp-in-string "\n$" "" (f-read-text "~/bin/dir0.txt" 'utf-8)))))
     ("!" "execute original '!'" shell-command)
     ]
-   ])
+   ]
+  )
+
 
 ;;;###autoload
 (defun magit-macro-list (command)
