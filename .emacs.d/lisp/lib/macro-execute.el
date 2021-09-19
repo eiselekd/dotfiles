a0ab2db5b81acb8f4544e36afdafb04f1599331c
Konrad Eisele <eiselekd@gmail.com>
2021-09-19 16:15:22 +0200

    all

--- .emacs.d/lisp/lib/macro-execute.el
+++ .emacs.d/lisp/lib/macro-execute.el
@@ -15,7 +15,11 @@
   :info-manual "(magit)Macros"
   ["Macros"
    [("0" "~/bin/la-1-2-r8.1-push-manifest.sh"
-     (lambda () (interactive) (magit-macro-list (format "~/bin/la-1-2-r8.1-push-manifest.sh %s" default-directory))))
+     (lambda () (interactive) (custom-execute "~/bin/la-1-2-r8.1-push-manifest.sh")))
+    ("1" "~/bin/la-1-2-r8.1-push-manifest.sh"
+     (lambda () (interactive) (custom-execute "~/bin/la-1-2-r8.1-push-manifest.sh")))
+    ("2" "~/bin/la-1-2-r8.1-push-manifest.sh"
+     (lambda () (interactive) (custom-execute "~/bin/la-1-2-r8.1-push-manifest.sh")))
     ("!" "switch dirs '!'" custom-dir-menue)
     ]
    ]
@@ -26,12 +30,23 @@
   :info-manual "(magit)Macros"
   ["Switchdirs"
    [("0" "~/bin/dir0.txt"
-     (lambda () (interactive) (dired (replace-regexp-in-string "\n$" "" (f-read-text "~/bin/dir0.txt" 'utf-8)))))
+     (lambda () (interactive) (custom-change-dir "~/bin/dir0.txt")))
+    ("1" "~/bin/dir1.txt"
+     (lambda () (interactive) (custom-change-dir "~/bin/dir1.txt")))
+    ("2" "~/bin/dir2.txt"
+     (lambda () (interactive) (custom-change-dir "~/bin/dir2.txt")))
     ("!" "execute original '!'" shell-command)
     ]
    ]
   )
 
+
+
+(defun custom-change-dir (d)
+  (dired (replace-regexp-in-string "\n$" "" (f-read-text d 'utf-8))))
+
+(defun custom-execute (f)
+  (magit-macro-list (format "%s %s" f default-directory)))
 
 ;;;###autoload
 (defun magit-macro-list (command)
@@ -41,6 +56,7 @@ Interactively, prompt for COMMAND in the minibuffer.  With a
 prefix argument COMMAND is run in the top-level directory of
 the current working tree, otherwise in `default-directory'."
   (interactive (list (magit-read-shell-command)))
+  (message "[+] Execute " command)
   (magit--shell-command command))
 
 (provide 'macro-execute)
